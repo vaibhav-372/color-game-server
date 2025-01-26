09a35bc8b9bcd2b3891cf8fb1bd8911c3b40ee1f
vaibhav-372 <vaibhavpv37@gmail.com>
2025-01-26 23:25:24 +0530

    payload

--- color-changer-server.js
+++ color-changer-server.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ limit: '10mb', extended: true }));
+app.use(express.urlencoded({ limit: '10mb', extended: true })); 
 
 // MongoDB connection
 const uri =
